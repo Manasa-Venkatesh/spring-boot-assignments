c53530c84c5dfcc690061b9ac86167e84c8fd223
Manasa Venkatesh <manasavenkatesh7@gmail.com>
2024-12-02 15:01:03 +0530

    first commit

---
+++ alice-bakery/src/main/java/com/manasa/springboot/week1/alice_bakery/frosting/StrawberryFrosting.java
@@ -0,0 +1,4 @@
+package com.manasa.springboot.week1.alice_bakery.frosting;
+
+public class StrawberryFrosting {
+}
---
+++ alice-bakery/src/main/java/com/manasa/springboot/week1/alice_bakery/syrup/Syrup.java
@@ -0,0 +1,4 @@
+package com.manasa.springboot.week1.alice_bakery.syrup;
+
+public interface Syrup {
+}
